bdfa118975923b9c5c0bee404b042b839855b19b
Roeland Matthijssens <fulgens.ailurus@gmail.com>
2014-08-17 15:45:36 +0200

    cleanup double folder structure

--- assets/js/helpMethods.js
+++
@@ -1,6 +0,0 @@
-function randomNumber (low, high) {
-	return Math.floor((Math.random()*high)+low);
-}
-String.prototype.capitalize = function() {
-    return this.charAt(0).toUpperCase() + this.slice(1);
-}
